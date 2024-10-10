21b34a46775cb7025d7ed2d645046c7f139ecf2c
Mia Dunn <miadunn@umich.edu>
2024-10-10 13:31:40 -0400

    added nuclear winter and updated docstrings

--- Lab 3/Lab03.py
+++ Lab 3/Lab03.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python 3
 '''
+This file performs N-Layer Atmosphere simulations for Lab 3. 
+To get solution for lab 3, run these commands:
+
+temp_emissivity()
+emissivity_layers()
+emissivity_layers(epsilon=1, S0=2600, goaltemp=700, albedo=0, venus=True)
+nuclear_winter()
 
 '''
 
@@ -9,7 +16,7 @@ import matplotlib.pyplot as plt
 # define constants
 sigma = 5.67*10**-8
 
-def n_layer_atm(N, epsilon, S0=1350, albedo=0.33, debug=False):
+def n_layer_atm(N, epsilon, S0=1350, albedo=0.33, debug=False, nuclearwinter=False):
     '''
     solve n layer atm problem and return temp at each layer
 
@@ -36,7 +43,14 @@ def n_layer_atm(N, epsilon, S0=1350, albedo=0.33, debug=False):
     # create matrices
     A = np.zeros([N+1, N+1])
     b = np.zeros(N+1)
-    b[0] = -S0/4 * (1-albedo)
+
+    # set initial conditions
+    if nuclearwinter:
+        # top layer is only afftected by incoming solar
+        b[-1] = -S0/4
+    else:
+        # surface layer is affected by incoing solar and the albedo of the surface
+        b[0] = -S0/4 * (1-albedo)
 
     if debug:
         print(f"Populating N+1 x N+1 matrix (N = {N})")
@@ -84,15 +98,10 @@ def temp_emissivity(N=1, de=0.05):
 
     Parameters
     ----------
-    N : int
-        Number of atmospheric layers, default=1
-    de : float
-        Change in emissivity values, default=0.05
-    
-    Returns
-    -------
-
-    
+    N : int, default=1
+        Number of atmospheric layers
+    de : float, default=0.05
+        Change in emissivity values
     '''
     # create arrary to hold temps
     temp_array = []
@@ -108,12 +117,15 @@ def temp_emissivity(N=1, de=0.05):
         # save the 1st temp value (sfc) and append it to the array
         temp_array.append(temps[0])
 
-    plt.plot(temp_array, emissivities, color='blue')
+    # plot
+    fig = plt.figure()
+    plt.plot(temp_array, emissivities)
     plt.xlabel('Surface Temperature (K)')
     plt.ylabel('Emissivity')
     plt.title('Surface Temperature vs Emissivity')
     plt.savefig('temp_emissivity.png')
 
+    # initialize arrays for finding the emissivity
     closest_temp = None
     closest_emissivity = None
     closest_diff = float('inf')  # Start with an infinitely large difference
@@ -130,11 +142,45 @@ def temp_emissivity(N=1, de=0.05):
 
     #return temp_array, emissivities
 
-def emissivity_layers(epsilon=0.255, S0=1350, albedo=0.33,maxlayers=50, tolerance=0.5, debug=False):
+def emissivity_layers(epsilon=0.255, S0=1350, albedo=0.33, maxlayers=50, tolerance=0.5,\
+                      goaltemp=288, debug=False, venus=False):
+    '''
+    For a set emissivity, increase the number of atmospheric layers until the 
+    surface temperature reaches a specified goal temperature.
+
+    Parameters
+    ----------
+    epsilon : float, default=0.255
+        Set the emissivity of the atmospheric layers
+    albedo : float, default=0.33
+        Set the planetary albedo from 0 to 1
+    S0 : float, default=1350
+        Set the incoming solar shortwave flux in Watts/m^2
+    maxlayers : int, default=50
+        The maximum number of atmospheric layers the function will loop through
+    tolerance : float, default=0.5
+        the tolerance level for the surface temperature to match the goal temperature
+    goaltemp : int, default=288 (K)
+        The surface temperature we want to produce with this function
+    debug : boolean, default=False
+        Turn on debug output
+    venus : boolean, default=False
+        Turns on venus output, used for question 4 of the lab description
+    
+    Returns
+    -------
+    N : int
+        Number of layers to reach goal temperature
+    surface_temp : int
+        The surface temperature calculated 
+    '''
+    # initialize
     N = 0
     temp_profile = []
 
+    # create loop through layers
     while N <= maxlayers:
+        # create and save temperature values
         temps = n_layer_atm(N=N, epsilon=epsilon, S0=S0, albedo=albedo, debug=debug)
         surface_temp = temps[0]
         temp_profile.append(temps)
@@ -143,23 +189,59 @@ def emissivity_layers(epsilon=0.255, S0=1350, albedo=0.33,maxlayers=50, toleranc
         if debug:
             print(f'N={N}, Surface Temperature={surface_temp}')
 
-        if abs(surface_temp - 288) <= tolerance:
-            print(f'N={N} with surface temp={surface_temp}')
-            #print(temp_profile[-1])
-            temp_heights = temp_profile[-1]
+        # for the temperature closest to the goal temp, print and plot results
+        if abs(surface_temp - goaltemp) <= tolerance:
+            if venus:
+                print(f'For Venus: N={N} with surface temp={surface_temp}')
+                return N, surface_temp
 
-            N_layers = np.arange(0, N+1, 1)
+            else:
+                print(f'N={N} with surface temp={surface_temp}')
+                #print(temp_profile[-1])
+                temp_heights = temp_profile[-1]
+                altitudes = np.arange(N+1)
 
-            plt.plot(temp_heights, N_layers)
-            plt.xlabel('Temperature (K)')
-            plt.ylabel('Altitude (N layers)')
-            plt.title('Temperature vs Altitude')
-            plt.savefig('emissivity_layers.png')
+                fig = plt.figure()
+                plt.plot(temp_heights, altitudes)
+                plt.xlabel('Temperature (K)')
+                plt.ylabel('Altitude (N layers)')
+                plt.title('Temperature vs Altitude')
+                plt.savefig('emissivity_layers.png')
 
-            return N, surface_temp, temp_heights
+                return N, surface_temp, temp_heights
 
+        # if the number of layers isn't enough to reach goal temp, increase N and repeat
         N += 1
-
 
     print(f'Maximum layers reached ({maxlayers}) without finding target temperature.')
     return None, None  # Return None if the maximum number of layers is reached without success
+
+def nuclear_winter(epsilon=0.5, S0=1350, N=5):
+    '''
+    The function runs the N-layer atmosphere model for a nuclear winter situation
+    and plots the output.
+
+    Parameters
+    ----------
+    N : int, default=5
+        Set the number of layers
+    epsilon : float, default=0.5
+        Set the emissivity of the atmospheric layers
+    S0 : float, default=1350
+        Set the incoming solar shortwave flux in Watts/m^2
+    '''
+    # run model and save results
+    temps = n_layer_atm(N=N, epsilon=epsilon, S0=S0, nuclearwinter=True)
+    altitudes = np.arange(N+1)
+
+    # plot 
+    fig = plt.figure()
+    plt.plot(temps, altitudes)
+    plt.xlabel('Temperature (K)')
+    plt.ylabel('Altitude (Number of Layers)')
+    plt.title('Temperature vs Altitude for Nuclear WInter')
+    plt.savefig('nuclear_winter.png')
+
+    # print surface temp
+    surface_temp = temps[0]
+    print('Surface Temp of Nuclear Winter Earth is:', surface_temp)
