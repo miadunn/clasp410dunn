d3b046bf4bb874b228bc817d2f14832d4d4a977e
Mia Dunn <miadunn@umich.edu>
2024-10-29 20:33:02 -0400

    added depth profile plot and updated comments

--- Lab4/Lab04.py
+++ Lab4/Lab04.py
@@ -1,14 +1,26 @@
-#1/usr/bin/env python3
+#!/usr/bin/env python3
 '''
-Tools and methods for solving our heat equation/diffusion
+This file performs the heat equation/diffusion simulations for Lab 4.
+To get the solution for lab 4, run these commands:
+
+example()
+permafrost(tmax=5*365*24*3600)
+permafrost()
+permafrost(temp_shift=0.5)
+permafrost(temp_shift=1)
+permafrost(temp_shift=3)
+
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def heatdiff(xmax, tmax, dx, dt, c2=1.0, tolerance=1e-3, debug=True, permafrost=False):
+def heatdiff(xmax, tmax, dx, dt, c2=1.0, debug=False,\
+              permafrost=False, temp_shift=0):
     '''
+    Calculates the heat diffusion matrix of specified dimensions (time and space)
+
     Parameters:
     -----------
     xmax : float
@@ -19,16 +31,30 @@ def heatdiff(xmax, tmax, dx, dt, c2=1.0, tolerance=1e-3, debug=True, permafrost=
         change in depth (meters)
     dt : float
         change in time (seconds)
-    c2 : float
+    c2 : float, default=1.0
         thermal diffusivity (m^2 / s)
-    tolerance : 
-    
-
+    debug : boolean, default=False
+        turns on/off debug option
+    permafrost : boolean, default=False
+        turns on/off permafrost option, which changes boundary conditions
+    temp_shift : float, default=0
+        temperature shift due to global warming
+        
     Returns:
     --------
+    xgrid : numpy array of size xmax*dx
+        array of ground depth values from the surface to a specified 
+        maximum depth (xmax) with a stepsize of dx
+
+    tgrid : numpy array of total time
+        array of the timeseries from 0 to tmax with stepsize dt (in seconds)
+
+    U : matrix of size tgrid x xgrid
+        matrix containing heat values (Celsius)
 
     '''
 
+    # 
     if dt > (dx**2 / (2*c2)):
         raise ValueError('dt is too large!')
 
@@ -38,6 +64,7 @@ def heatdiff(xmax, tmax, dx, dt, c2=1.0, tolerance=1e-3, debug=True, permafrost=
 
     xgrid, tgrid = np.arange(0, xmax+dx, dx), np.arange(0, tmax+dt, dt)
 
+    # set debug statements
     if debug:
         print(f'Our grid goes from 0 to {xmax}m and 0 to {tmax}s')
         print(f'Our spatial step is {dx} and time step is {dt}')
@@ -57,69 +84,127 @@ def heatdiff(xmax, tmax, dx, dt, c2=1.0, tolerance=1e-3, debug=True, permafrost=
         U[:, 0] = 4*xgrid - 4*xgrid**2
     
     if permafrost==True:
-        U[0, :] = temp_kanger(tgrid/(60*60*24)) # tgrid in days
+        U[0, :] = temp_kanger(tgrid/(60*60*24), temp_shift) # tgrid in days
         U[-1, :] = 5
         U[:, 0] = 0
 
     # Set our "r" constant.
     r = c2 * dt / dx**2
 
+    # make steady state check
+    steps_per_year = int(365*24*3600/dt)
+    steady_state_year = None
+
     # Solve! Forward differnce ahoy.
     for j in range(N-1):
         U[1:-1, j+1] = (1-2*r) * U[1:-1, j] + \
             r*(U[2:, j] + U[:-2, j])
         
-        # set limit for tolerance so the function doesn't run longer than needed
-        max_change = np.max(np.abs(U[:,j+1] - U[:, j]))
-        # 
-        if max_change < tolerance:
-            print(f'Steady state reached after {j * dt / (60 * 60 * 24 * 365):.1f} years')
-            break
-
-
     # Return grid and result:
     return xgrid, tgrid, U
 
 def example(xmax=1, tmax=0.2, dx=0.2, dt=0.02, permafrost=False):
+    '''
+    Replicates the example in the lab description and produces a table to compare
+
+    Parameters:
+    -----------
+    xmax : float, default=1
+        maximum depth (meters)
+    tmax : float, default=0.2
+        maximum time (seconds)
+    dx : float, default=0.2
+        change in depth (meters)
+    dt : float, defaut=0.02
+        change in time (seconds)
+    permafrost : boolean, default=False
+    '''
+    # calculate heat matrix
     x, time, heat = heatdiff(xmax, tmax, dx, dt, permafrost=permafrost)
 
+    # round values for simplicity 
     rounded_heat = np.round(heat, 7)
+
+    # create table with heat values
+    fig = plt.figure(figsize=(10,8))
     row_labels = [f"{i}" for i in range(heat.shape[0])]
     col_labels = [f"{j}" for j in range(heat.shape[1])]
-
-    fig = plt.figure(figsize=(10,8))
     plt.axis('off')
     table = plt.table(cellText = rounded_heat, loc='center', rowLabels=row_labels,\
                     colLabels=col_labels)
     table.scale(1.2,1.2)
     table.auto_set_font_size(False)
     table.set_fontsize(10)
-    plt.show()
+    plt.savefig('example_table.png')
 
-    print()
-    print(heat)
+    #print()
+    #print(heat)
 
-def temp_kanger(t):
+def temp_kanger(t, temp_shift=0):
     '''
     for an array of times in days, return timeseries of surface 
     temperature for Kangerlussuaq, Greenland
+
+    Parameters:
+    -----------
+    t : numpy array of total time
+        array of the timeseries in days 
+    temp_shift : float, default=0
+        the temperature shift in the case of global warming conditions
+        
+    Returns:
+    --------
+    array of surface temperatures for the given timeseries
+
     '''
     # monthly avg temps
     t_kanger = np.array([-19.7, -21.0, -17.0, -8.4, 2.3, 8.4,
                          10.7, 8.5, 3.1, -6.0, -12.0, -16.9])
     
+    # add the temperature shift to the monthly avg temps
+    t_kanger = t_kanger + temp_shift
+    
+    # calculate and return the sinusoidal temperature function
     t_amp = (t_kanger - t_kanger.mean()).max()
     return t_amp*np.sin(((np.pi/180) * t) - (np.pi/2)) + t_kanger.mean()
 
-def permafrost(xmax=100, tmax=(100*365*24*60*60), dx=0.5, dt=24*3600, c2=2.5e-7, tolerance=1e-3, permafrost=True):
+def permafrost(xmax=100, tmax=(100*365*24*60*60), dx=0.5, dt=24*3600, c2=2.5e-7,\
+                permafrost=True, temp_shift=0):
+    '''
+    For surface temperatures in Kangerlussuaq, calculates the heat diffusion matrix and plots it.
+    Also, plots the temperature profile for summer and winter conditions, and prints the depth of 
+    the active and permafrost layers.
+
+    Parameters:
+    -----------
+    xmax : float
+        maximum depth (meters)
+    tmax : float
+        maximum time (seconds)
+    dx : float
+        change in depth (meters)
+    dt : float
+        change in time (seconds)
+    c2 : float, default=1.0
+        thermal diffusivity (m^2 / s)
+    permafrost : boolean, default=False
+        turns on/off permafrost option, which changes boundary conditions
+    temp_shift : float, default=0
+        temperature shift due to global warming
     
-    x, time, heat = heatdiff(xmax=xmax, tmax=tmax, dx=dx, dt=dt, c2=c2, tolerance=tolerance, permafrost=permafrost)
+    '''
+    # calculate heat matrix
+    x, time, heat = heatdiff(xmax=xmax, tmax=tmax, dx=dx, dt=dt, c2=c2,\
+                            permafrost=permafrost, temp_shift=temp_shift)
 
     # plot colormap
     fig, axs = plt.subplots(1,2, figsize=(15,6))
     map = axs[0].pcolor((time/ (60*60*24*365)), x, heat, cmap='seismic', vmin=-25, vmax=25)
     axs[0].invert_yaxis()
     plt.colorbar(map, ax=axs[0], label='Temperature ($C$)')
+    axs[0].set_xlabel('Time (years)')
+    axs[0].set_ylabel('Depth (m)')
+    axs[0].set_title('Ground Temperature in Kangerlussuaq, Greenland')
 
     # get winter/summer profile values
     winter = heat[:, -365:].min(axis=1)
@@ -130,9 +215,12 @@ def permafrost(xmax=100, tmax=(100*365*24*60*60), dx=0.5, dt=24*3600, c2=2.5e-7,
     axs[1].plot(winter, x, color='blue', label='winter')
     axs[1].invert_yaxis()
     axs[1].legend()
+    axs[1].set_xlabel('Temperature ($C$)')
+    axs[1].set_ylabel('Depth (m)')
+    axs[1].set_title('Ground Temperature in Kangerlussuaq, Greenland')
 
     plt.tight_layout()
-    plt.savefig('permafrost.png')
+    plt.savefig(f'permafrost{temp_shift}.png')
     plt.close('all')
 
     # calculate active and permafrost layer depth
@@ -143,7 +231,4 @@ def permafrost(xmax=100, tmax=(100*365*24*60*60), dx=0.5, dt=24*3600, c2=2.5e-7,
 
     print()
     print('Depth of active layer:', active_layer_depth, 'm')
-    print()
-    print('Depth of the permafrost layer is:', permafrost_layer_depth, 'm')
-
-
+    print('Depth of the permafrost layer:', permafrost_layer_depth, 'm')
